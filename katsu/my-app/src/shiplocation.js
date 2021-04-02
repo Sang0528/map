dfd3c334e0ac9556e392b94e7c3f4797f4dc7836
Katsu <sang@e-technostar.com>
2021-04-02 17:52:16 +0700

    modify pic to last position

--- katsu/my-app/src/shiplocation.js
+++ katsu/my-app/src/shiplocation.js
@@ -1,12 +1,6 @@
 import * as React from "react";
 import { Marker } from "react-map-gl";
 
-const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
-  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
-  C20.1,15.8,20.2,15.8,20.2,15.7z`;
-
-const SIZE = 20;
-
 // Important for perf: the markers never change, avoid rerender when the map viewport changes
 function ShipLocationPins(props) {
   const { data, onClick } = props;
@@ -14,10 +8,10 @@ function ShipLocationPins(props) {
   return data.map((ship, index) => (
     <Marker
       key={`marker-${index}`}
-      longitude={ship.longitude}
-      latitude={ship.latitude}
+      longitude={ship.Coordinates[ship.Coordinates.length - 1][0]}
+      latitude={ship.Coordinates[ship.Coordinates.length - 1][1]}
       offsetLeft = {-10}
-      offsetTop = {-30}
+      offsetTop = {-20}
     >
       <img 
         width = {30}
